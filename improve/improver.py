df55d02aa6f22571e0536e62c7bb7ae3f0bc66ba
Andre Schidler <andre@schidler.at>
2020-07-15 17:34:21 +0200

    Started new strategy

--- improve/improver.py
+++ improve/improver.py
@@ -1,3 +1,6 @@
+import sys
+from collections import defaultdict
+
 import bdd_instance
 import sat_tools
 from tree_depth_encoding import TreeDepthEncoding
@@ -92,7 +95,47 @@ def replace(old_tree, new_tree, root):
     # Sub-tree is now been added in place of the old sub-tree
 
 
-def leaf_rearrange(tree, instance, limit=15):
+def build_unique_set(root, samples, examples, limit=sys.maxsize):
+    c_features = set()
+    c_leafs = []
+
+    q = [(root, 0)]
+    climit = limit
+    depth = 0
+
+    while q and climit >= 0:
+        c_q, d = q.pop()
+        depth = max(depth, d)
+        climit -= 1
+        if not c_q.is_leaf and climit > 0:
+            q.append((c_q.left, d+1))
+            q.append((c_q.right, d+1))
+            c_features.add(c_q.feature)
+        else:
+            c_leafs.append(c_q.id)
+
+    feature_map = {}
+    c_features = list(c_features)
+    for i in range(1, len(c_features) + 1):
+        feature_map[i] = c_features[i - 1]
+
+    new_instance = bdd_instance.BddInstance()
+    added = set()
+    for s in samples:
+        values = [None for _ in range(0, len(feature_map) + 1)]
+        for k, v in feature_map.items():
+            values[k] = examples[s].features[v]
+
+        tp = tuple(values)
+        if tp not in added:
+            added.add(tp)
+            new_instance.add_example(
+                bdd_instance.BddExamples(values, examples[s].cls, examples[s].id))
+
+    return new_instance, feature_map, c_leafs, depth
+
+
+def leaf_rearrange(tree, instance, depth_limit=15, sample_limit=200):
     runner = sat_tools.SatRunner(TreeDepthEncoding, sat_tools.GlucoseSolver(), base_path=".")
     assigned = assign_samples(tree, instance)
     structure = find_structure(tree)
@@ -105,47 +148,27 @@ def leaf_rearrange(tree, instance, limit=15):
         except ValueError:
             break
 
-        climit = limit
+        climit = depth_limit
         c_parent = max_node
+        c_depth = 0
+        last_instance = None
+        last_parent = None
 
         # Find parent of the subtree
         while climit > 0 and c_parent[2] is not None:
             c_parent = structure[c_parent[2]]
+            c_instance = build_unique_set(c_parent[0], assigned[c_parent[0].id], instance.examples)
+            if len(c_instance[0].examples) > sample_limit:
+                break
+            last_parent = c_parent
+            last_instance = c_instance
             climit -= 1
+            c_depth += 1
 
-        # Now traverse and find involved leaves as well as features
-        c_samples = assigned[c_parent[0].id]
-        c_depth = limit - climit
-        c_features = set()
-        c_leafs = []
+        if last_instance is None or c_depth <= 1:
+            continue
 
-        q = [c_parent[0]]
-        while q:
-            c_q = q.pop()
-            if not c_q.is_leaf:
-                q.append(c_q.left)
-                q.append(c_q.right)
-                c_features.add(c_q.feature)
-            else:
-                c_leafs.append(c_q.id)
-
-        # Now create a new instance
-        feature_map = {}
-        c_features = list(c_features)
-        for i in range(1, len(c_features)+1):
-            feature_map[i] = c_features[i-1]
-
-        new_instance = bdd_instance.BddInstance()
-        added = set()
-        for s in c_samples:
-            values = [None for _ in range(0, len(feature_map) + 1)]
-            for k, v in feature_map.items():
-                values[k] = instance.examples[s].features[v]
-
-            tp = tuple(values)
-            if tp not in added:
-                added.add(tp)
-                new_instance.add_example(bdd_instance.BddExamples(values, instance.examples[s].cls, len(new_instance.examples)))
+        new_instance, feature_map, c_leafs, _ = last_instance
 
         # Solve instance
         new_tree, _ = runner.run(new_instance, c_depth - 1, u_bound=c_depth - 1)
@@ -165,10 +188,11 @@ def leaf_rearrange(tree, instance, limit=15):
                     q.append(c_q.right)
 
             # Clean tree
-            replace(tree, new_tree, c_parent[0])
+            replace(tree, new_tree, last_parent[0])
             structure = find_structure(tree)
-            print(f"New tree: {new_tree.get_depth()} / {limit}")
-            print(f"Finished sub-tree, improvement, acc {tree.get_accuracy(instance.examples)}, depth {tree.get_depth()}, root {c_parent[0].id}")
+            assigned = assign_samples(tree, instance)
+            print(f"New tree: {new_tree.get_depth()} / {c_depth}")
+            print(f"Finished sub-tree, improvement, acc {tree.get_accuracy(instance.examples)}, depth {tree.get_depth()}, root {last_parent[0].id}")
 
 
 def leaf_select(tree, instance, sample_limit=50, depth_limit=12):
@@ -204,3 +228,45 @@ def leaf_select(tree, instance, sample_limit=50, depth_limit=12):
                     f"Finished sub-tree, improvement, acc {tree.get_accuracy(instance.examples)}, depth {tree.get_depth()}, root {c_r.id}")
 
         # No need to retry, as the number of assigned samples stayed the same
+
+
+def mid_rearrange(tree, instance, sample_limit=50, depth_limit=12):
+    assigned = assign_samples(tree, instance)
+    runner = sat_tools.SatRunner(TreeDepthEncoding, sat_tools.GlucoseSolver(), base_path=".")
+
+    for i in range(0, len(tree.nodes)):
+        if tree.nodes[i] is None or tree.nodes[i].is_leaf:
+            continue
+
+        c_parent = tree.nodes[i]
+        last_instance = None
+
+        for r in range(1, depth_limit+1):
+            c_instance = build_unique_set(c_parent, assigned[c_parent.id], instance.examples, r)
+
+            if len(c_instance[0].examples) > sample_limit:
+                break
+
+            last_instance = c_instance
+
+        if last_instance[3] <= 1:
+            continue
+
+        class_mapping = {}
+        reverse_mapping = defaultdict(list)
+        for cl in last_instance[2]:
+            for al in assigned[cl]:
+                class_mapping[al] = cl
+                reverse_mapping[cl].append(al)
+
+        for ex in last_instance[0].examples:
+            ex.cls = class_mapping[ex.id]
+
+        new_tree, _ = runner.run(last_instance[0], last_instance[3] - 1, u_bound=last_instance[3] - 1)
+
+        if new_tree is not None:
+            print(f"Found one {new_tree.get_depth()}/{last_instance[3]}, root {c_parent.id}")
+        else:
+            print(f"Not found {last_instance[3]}, root {c_parent.id}")
+
+    # TODO: Do the same thing with feature reduction?
